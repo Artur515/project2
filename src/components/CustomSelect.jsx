aec2e833cfa2e6097f595836bfca7cfcb9c8065c
Arthur <drugartur5@gmail.com>
2021-11-18 11:50:18 +0200

    fix customSelect

--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -3,7 +3,8 @@ import {Select} from "antd";
 
 const {Option} = Select
 
-const CustomSelect = ({placeholder, options, value, ...props}) => {
+const
+    CustomSelect = ({placeholder, options, value, ...props}) => {
 
     return (
         <Select
@@ -11,12 +12,7 @@ const CustomSelect = ({placeholder, options, value, ...props}) => {
             style={{minWidth: 200, margin: props.style ? props.style : 15}}
             placeholder={placeholder}
             defaultValue={value ? value : placeholder}
-            showSearch
-            optionFilterProp="children"
-            filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-            filterSort={(optionA, optionB) =>
-                optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())}>
+            showSearch>
             {options.map((option, index) => <Option key={index} value={option}>{option}</Option>)}
         </Select>
     );
